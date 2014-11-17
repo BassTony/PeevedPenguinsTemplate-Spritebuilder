d508b1759cd9f03f72e207251109631e12587a0d
Toni Mäki-Leppilampi <toni@MacMini.Elisa>
2014-11-17 21:49:37 +0200

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Perhonen.h
@@ -0,0 +1,13 @@
+//
+//  Perhonen.h
+//  PeevedPenguins
+//
+//  Created by Toni Mäki-Leppilampi on 17.11.2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Perhonen : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Perhonen.m
@@ -0,0 +1,18 @@
+//
+//  Perhonen.m
+//  PeevedPenguins
+//
+//  Created by Toni Mäki-Leppilampi on 17.11.2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Perhonen.h"
+
+@implementation Perhonen
+
+-(void)didLoadFromCCB
+{
+    TRUE;
+}
+
+@end
