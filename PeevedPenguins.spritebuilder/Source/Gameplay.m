8ea7c94240335a686d6760ac9e0db808579c8c34
Toni Mäki-Leppilampi <toni@MacMini.Elisa>
2014-11-22 09:13:55 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -26,7 +26,7 @@
 }
 
 static const float MIN_SPEED = 50.f;
-static const float GRAVITY = -300.f;
+static const float GRAVITY = -100.f;
 
 - (void)didLoadFromCCB {
     self.userInteractionEnabled = TRUE;
